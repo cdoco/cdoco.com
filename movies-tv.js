6fb3964526d4f1661fe16ee493b41b8c7dcb62fa
ZiHang Gao <gao_zihang@hotmail.com>
2023-09-24 21:30:08 +0800

    Update movies-tv.js

--- movies-tv.js
+++ movies-tv.js
@@ -15,59 +15,16 @@ class MoviesTv extends Deup {
     name: 'Movies & TV',
     layout: 'poster',
     timeout: 10000,
-    pageSize: 50,
+    pageSize: 20,
   };
 
   /**
-   * Define inputs
+   * Base url
    *
-   * Types:
-   * - 1: 电影
-   * - 2: 电视剧
-   * - 3: 综艺
-   * - 4: 动漫
-   * - 6: 动作
-   * - 7: 喜剧
-   * - 8: 爱情
-   * - 9: 科幻
-   * - 10: 恐怖
-   * - 11: 剧情
-   * - 12: 战争
-   * - 13: 国产剧
-   * - 14: 香港剧
-   * - 15: 韩国剧
-   * - 16: 欧美剧
-   * - 20: 记录片
-   * - 21: 台湾剧
-   * - 22: 日本剧
-   * - 23: 海外剧
-   * - 24: 泰国剧
-   * - 25: 大陆综艺
-   * - 26: 港台综艺
-   * - 27: 日韩综艺
-   * - 28: 欧美综艺
-   * - 29: 国产动漫
-   * - 30: 日韩动漫
-   * - 31: 欧美动漫
-   * - 32: 港台动漫
-   * - 33: 海外动漫
-   * - 34: 伦理片
-   * - 35: 电影解说
-   * - 36: 体育
-   * - 37: 足球
-   * - 38: 篮球
-   * - 39: 网球
-   * - 40: 斯诺克
-   *
-   * @type {{type: {label: string, placeholder: string, required: boolean}}}
+   * @type {string}
+   * @private
    */
-  inputs = {
-    type: {
-      label: '类别',
-      required: false,
-      placeholder: '默认为全部类别, 详情请查看源码注释, eg: 1',
-    },
-  };
+  _baseUrl = 'https://www.hongniuzy2.com/api.php/provide/vod/from/hnm3u8';
 
   /**
    * Check inputs
@@ -102,38 +59,12 @@ class MoviesTv extends Deup {
    * @param limit
    * @returns {Promise<*>}
    */
-  async list(object = null, offset = 0, limit = 50) {
-    const page = Math.floor(offset / limit) + 1;
-    const type = (await $storage.inputs).type || '';
-    let url = `https://cj.lziapi.com/api.php/provide/vod?ac=detail&pg=${page}&pagesize=${limit}`;
-
-    // Filter by type
-    if (type !== '') {
-      const $ = $cheerio.load(
-        (
-          await $axios.get(
-            `http://lzizy.net/index.php/vod/type/id/${type}/page/${page}.html`,
-          )
-        ).data,
-      );
-      const ids = $('ul.videoContent')
-        .children()
-        .map((index, element) => {
-          const url = $(element).find('a.videoName').attr('href');
-          return url?.match(/\/(?<id>\d+)\.html/)?.groups?.id;
-        })
-        .get()
-        .join(',');
-
-      if (ids === '') {
-        $alert('未发现该类别的视频, 请查看源码注释获取类别编号');
-        return [];
-      }
-      url = `https://cj.lziapi.com/api.php/provide/vod?ac=detail&ids=${ids}&pagesize=${limit}`;
-    }
-
+  async list(object = null, offset = 0, limit = 20) {
     try {
-      const response = await $axios.get(url);
+      const page = Math.floor(offset / limit) + 1;
+      const response = await $axios.get(
+        `${this._baseUrl}?ac=detail&pg=${page}&pagesize=${limit}`,
+      );
       return this.formatVideoList(response.data);
     } catch (e) {
       $alert(e.message);
@@ -155,7 +86,7 @@ class MoviesTv extends Deup {
     try {
       const page = Math.floor(offset / limit) + 1;
       const response = await $axios.get(
-        `https://cj.lziapi.com/api.php/provide/vod?ac=detail&wd=${keyword}&pg=${page}&pagesize=${limit}`,
+        `${this._baseUrl}?ac=detail&wd=${keyword}&pg=${page}&pagesize=${limit}`,
       );
 
       return this.formatVideoList(response.data);
